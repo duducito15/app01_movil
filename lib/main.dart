7ad680b5430ebf67d1098c281cdf88f2ac5daee9
Eduardo Chavez <echavezv@utea.edu.pe>
2022-10-03 13:03:37 -0500

    Agregado cards e iconos

--- lib/main.dart
+++ lib/main.dart
@@ -16,15 +16,18 @@ class MyApp extends StatelessWidget {
             CircleAvatar(
               radius: 60.0,
               backgroundColor: Colors.white,
-              backgroundImage: NetworkImage(
-                  "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg"),
+              backgroundImage: AssetImage("assets/images/persona2.jpeg"),
+            ),
+            SizedBox(
+              height: 30.0,
             ),
             Text(
               "Fiorela de Fátima Guadalupe",
               style: TextStyle(
                 color: Colors.white,
                 fontSize: 25.0,
-                fontWeight: FontWeight.w500,
+                fontWeight: FontWeight.bold,
+                fontFamily: "Dancing",
               ),
             ),
             Text(
@@ -35,7 +38,60 @@ class MyApp extends StatelessWidget {
                 fontWeight: FontWeight.w300,
                 letterSpacing: 2.0,
               ),
-            )
+            ),
+            Divider(
+              thickness: 0.70,
+              color: Colors.white38,
+              indent: 120.0,
+              endIndent: 120.0,
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
+              elevation: 10.0,
+              child: ListTile(
+                title: Text("+51 983707845"),
+                subtitle: Text("Teléfono"),
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.indigo,
+                ),
+                trailing: Icon(
+                  Icons.check_circle_outline,
+                  color: Colors.indigo,
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
+              elevation: 10.0,
+              child: ListTile(
+                title: Text("eduardochavezv@gmail.com"),
+                subtitle: Text("Correo electrónico"),
+                leading: Icon(
+                  Icons.mail,
+                  color: Colors.indigo,
+                ),
+                trailing: Icon(
+                  Icons.check_circle_outline,
+                  color: Colors.indigo,
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                Icon(Icons.alarm),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Icon(Icons.alarm),
+                Icon(Icons.alarm),
+              ],
+            ),
           ],
         ),
       ),
